974cda23fb3c166818811ff737b7f20feb78e64f
Lukas Taake <lukas.taake@holisticon.de>
2018-06-01 16:23:43 +0200

    Fixed integration tests (issue #3) by adding @RequestParam to method parameters. Converted ApplicationController to kotlin

--- backend/src/test/java/de/holisticon/reference/rest/api/ApplicationControllerTest.java
+++ backend/src/test/java/de/holisticon/reference/rest/api/ApplicationControllerTest.java
@@ -1,18 +1,22 @@
 package de.holisticon.reference.rest.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import de.holisticon.reference.data.ApplicationConverter;
+import de.holisticon.reference.data.ApplicationRepository;
 
 public class ApplicationControllerTest {
 
     private ApplicationController controller;
+
     @Before
     public void init () {
-        controller = new ApplicationController();
+        controller = new ApplicationController(mock(ApplicationRepository.class), mock(ApplicationConverter.class));
     }
-
 
     @Test
     public void shouldCreateApplication(){
